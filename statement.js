006b2285f440b176012f6d1ac3708ef26e184f5c
SeungrokYoon <sonicrok93@gmail.com>
2024-05-19 08:51:45 +0900

    :sparkles: Feat :  switch문과 format 함수를 추출

--- statement.js
+++ statement.js
@@ -1,48 +1,47 @@
+const formatCurrency = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+}).format;
 const getResultTitleStr = (customer) => `청구 내역 (고객명 : ${customer})\n`;
 const getTotalAmountStr = (totalAmount) =>
-  `총액: ${format(totalAmount / 100)}\n`;
+  `총액: ${formatCurrency(totalAmount / 100)}\n`;
 const getVolumeCreditsStr = (credits) => `적립 포인트: ${credits}점\n`;
+
+const calcAmountFor = (plays, perf) => {
+  const play = plays[perf.playID];
+  let thisAmount = 0;
+
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+
+  return thisAmount;
+};
 
 export default function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = getResultTitleStr(invoice.customer);
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-    //포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    //희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-    //청구 내역을 출력한다.
-    result += `${play.name}: ${format(thisAmount / 10)} ${perf.audience}석\n`;
-    totalAmount += thisAmount;
+    totalAmount += calcAmountFor(plays, perf);
   }
+
   result += getTotalAmountStr(totalAmount);
   result += getVolumeCreditsStr(volumeCredits);
   return result;
