41590e6d2234b42dc56806baa0999622e74a8726
SeungrokYoon <sonicrok93@gmail.com>
2024-05-26 20:37:57 +0900

    feat: lodash.curry 를 통한 partial 커링

--- utils/Currying.js
+++ utils/Currying.js
@@ -1,3 +1,5 @@
+import lodashCurry from "lodash.curry";
+
 function curry(curryingTargetFunction) {
   return function (aArgAfterFunctionDeclaration) {
     return function (bArgAfterFunctionDeclaration) {
@@ -14,8 +16,23 @@ function sum(a, b) {
 }
 
 const curriedSum = curry(sum);
-console.log(curriedSum(1)(2));
 /**
  * console.log(curriedSum(1)(2)(3)); 이렇게 쓰면 에러가 난다.
  * 왜냐하면 헬퍼함수인 curry 함수가 리턴을 두 번밖에 못하기 때문이다.
  *  */
+console.log(curriedSum(1)(2));
+
+function log(date, importance, message) {
+  console.log(
+    `[${date.getHours()}:${date.getMinutes()}] [${importance}] ${message}`
+  );
+}
+
+//lodash curry
+log = lodashCurry(log);
+log(new Date(), "DEBUG", "some debug");
+log(new Date())("DEBUG")("some debug");
+
+//partial 적용된 함수 - partial
+const logNow = log(new Date());
+logNow("INFO")("message info");
