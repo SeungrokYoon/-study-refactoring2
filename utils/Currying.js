b63b374fdb11e49e7c38dc0e4554f75600bb9e41
SeungrokYoon <sonicrok93@gmail.com>
2024-05-26 20:01:00 +0900

    feat: 기본적인 커리함수의 한계점 - 몇 개의 인자가 들어올 지 모른다

--- utils/Currying.js
+++ utils/Currying.js
@@ -8,3 +8,14 @@ function curry(curryingTargetFunction) {
     };
   };
 }
+
+function sum(a, b) {
+  return a + b;
+}
+
+const curriedSum = curry(sum);
+console.log(curriedSum(1)(2));
+/**
+ * console.log(curriedSum(1)(2)(3)); 이렇게 쓰면 에러가 난다.
+ * 왜냐하면 헬퍼함수인 curry 함수가 리턴을 두 번밖에 못하기 때문이다.
+ *  */
